77da700ecb8b1d0e52e17442a95c7644d2cce4a9
rafaelalvarezmt <rafael.alvarez.mt@usa.edu.co>
2023-08-17 21:09:41 -0500

    Version 1.3.3

--- pkgdir/control/ControllerMediaAdmin.java
+++ pkgdir/control/ControllerMediaAdmin.java
@@ -18,14 +18,37 @@ import java.io.File;
 import java.nio.file.Files;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+import javazoom.spi.vorbis.sampled.file.VorbisAudioFileReader;
+import javazoom.spi.mpeg.sampled.file.MpegAudioFileReader;
+import org.tritonus.share.sampled.file.TAudioFileReader;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.Clip;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequencer;
 
 public class ControllerMediaAdmin implements ActionListener{
 
+	private int BUFFER_SIZE = 128000;
 	private GuiMenu guiMenul;
 	public GuiMediaAdmin guiMediaAdminl;
 	private File[] sel_files;
 	private String currentdir;		
+	private AudioInputStream in;	
+	private AudioFormat baseFormat;
+	private Clip clip;
+	private Sequencer sequencer;
 
 	/**
      * Constructor sin parametros
@@ -63,7 +86,7 @@ public class ControllerMediaAdmin implements ActionListener{
 					File sel_file = guiMediaAdminl.getFileChooser().getSelectedFile();
 					String n_msource = sel_file.getParent()+"/"+sel_file.getName();
 					currentdir = sel_file.getParent();
-					System.out.println("currentdir: " + currentdir);
+					System.out.println("currentdir: " + currentdir+" n_msource: "+n_msource);
 					guiMediaAdminl.getLabelFileName().setText( n_msource );
 					guiMediaAdminl.getMediaContainer().setVisible(true);														
 					guiMediaAdminl.getButtonsJPanel().setVisible(true);
@@ -90,8 +113,12 @@ public class ControllerMediaAdmin implements ActionListener{
 						|| mimeType.equals( "video/x-msvideo"  ) ){
 							System.out.println("Soy un video." );
 					}else if( mimeType.equals( "audio/mpeg" ) 
-						|| mimeType.equals( "audio/x-vorbis+ogg" ) ){
-							System.out.println("Soy un audio." );
+						|| mimeType.equals( "audio/x-vorbis+ogg" )
+						|| mimeType.equals( "audio/ogg" )
+						|| mimeType.equals( "audio/x-wav" )
+						|| mimeType.equals( "audio/midi" )  ){
+							System.out.println("Soy un audio: "+mimeType );
+						playSound( mimeType, n_msource );
 					}else{
 						System.out.println("Un desconocido." );
 					}						
@@ -111,6 +138,12 @@ public class ControllerMediaAdmin implements ActionListener{
 		* Evento sobre boton Cancelar
 		*/
 		if( ae.getSource() == guiMediaAdminl.getBotonBack() ){
+			if( clip != null ){
+				clip.close();
+			}
+			if( sequencer != null ){
+				sequencer.stop();
+			}
 			//Remueve el boton flotante sobre la multimedia
 			guiMenul.getMainJFrame().getContentPane().remove( guiMediaAdminl.getButtonsJPanel() );
 			guiMediaAdminl.getFileJPanel().removeAll();
@@ -137,7 +170,7 @@ public class ControllerMediaAdmin implements ActionListener{
      */
 	private void agregarEventos(){
 		guiMenul.getItemMedia().addActionListener(this);
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("Multimedia", "mp4", "mkv", "avi","jpg","png","ogg","mp3","wav");
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Multimedia", "mp4", "mkv", "avi","jpg","jpeg","png","ogg","mp3","wav","mid");
           guiMediaAdminl.getFileChooser().setAcceptAllFileFilterUsed(false);
 		guiMediaAdminl.getFileChooser().addChoosableFileFilter(filter);
 		guiMediaAdminl.getFileChooser().addActionListener(this);
@@ -182,4 +215,70 @@ public class ControllerMediaAdmin implements ActionListener{
 		}
 		return resizeData;
 	}
+
+	private void playSound( String mimeType, String pathm ){
+		try{
+			switch (mimeType) {
+				case "audio/midi":
+					sequencer = MidiSystem.getSequencer();
+					sequencer.open();
+					InputStream is = new BufferedInputStream(new FileInputStream(new File( pathm )));
+					sequencer.setSequence(is);
+					sequencer.start();
+				 	break;
+				case "audio/x-wav":
+					in = AudioSystem.getAudioInputStream( new File( pathm ) );
+					baseFormat = in.getFormat();
+      				AudioFormat targetFormatw = new AudioFormat(
+					    AudioFormat.Encoding.PCM_SIGNED,
+					    baseFormat.getSampleRate(), 16,  baseFormat.getChannels(),
+					    baseFormat.getChannels() * 2,  baseFormat.getSampleRate(), false
+					);
+					AudioInputStream audioInputStreamw = AudioSystem.getAudioInputStream(targetFormatw, in);
+					clip = AudioSystem.getClip();
+					clip.open(audioInputStreamw);
+					clip.start();
+					in.close();
+				 	break;
+				case "audio/mpeg":
+					MpegAudioFileReader mp=new MpegAudioFileReader();
+					in=mp.getAudioInputStream( new File( pathm ) );
+					AudioFormat baseFormatm = in.getFormat();
+      				AudioFormat targetFormatm=new AudioFormat(
+					    AudioFormat.Encoding.PCM_SIGNED,
+					    baseFormatm.getSampleRate(), 16,  baseFormatm.getChannels(),
+					    baseFormatm.getChannels() * 2,  baseFormatm.getSampleRate(), false
+					);
+					AudioInputStream audioInputStreamm = AudioSystem.getAudioInputStream(targetFormatm, in);
+					clip = AudioSystem.getClip();
+					clip.open(audioInputStreamm);
+					clip.start();
+					in.close();
+				 	break;
+				case "audio/ogg":
+					VorbisAudioFileReader vb=new VorbisAudioFileReader();
+					in=vb.getAudioInputStream( new File( pathm ) );
+					baseFormat=in.getFormat();
+      				AudioFormat targetFormat=new AudioFormat(
+					    AudioFormat.Encoding.PCM_SIGNED,
+					    baseFormat.getSampleRate(), 16,  baseFormat.getChannels(),
+					    baseFormat.getChannels() * 2,  baseFormat.getSampleRate(), false
+					);
+					AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(targetFormat, in);
+					clip = AudioSystem.getClip();
+					clip.open(audioInputStream);
+					clip.start();
+					in.close();
+				 	break;
+			}
+			URL url = GuiMenu.class.getResource("../../res/midi_graph_m.jpeg");
+		     BufferedImage img = ImageIO.read(url);
+			ImageIcon imgIcon = new ImageIcon( img );
+			guiMediaAdminl.getMediaContainer().add( new JLabel( imgIcon ) );						
+			
+		}catch( Exception e){
+			System.out.println( e.toString() );
+		}	
+	}
+
 }
--- pkgdir/java_build.sh
+++ pkgdir/java_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 echo "Compilando paquetes"
-javac -cp .:dist/lib/mysql-connector-java-8.0.11.jar:dist/lib/zip4j-2.11.2.jar:dist/lib/commons-net-1.2.1.jar CargarClase.java graficos/*.java control/*.java modelo/*.java -d ./dist/
+javac -cp .:dist/lib/mysql-connector-java-8.0.11.jar:dist/lib/zip4j-2.11.2.jar:dist/lib/commons-net-1.2.1.jar:dist/lib/jlayer-1.0.1.jar:dist/lib/vorbisspi-1.0.3.3.jar:dist/lib/tritonus-share-0.3.7.4.jar:dist/lib/mp3spi-1.9.5.4.jar CargarClase.java graficos/*.java control/*.java modelo/*.java -d ./dist/
 echo "Generando JAR"
 cd dist/
 jar -cmf META-INF/MANIFEST.MF java_clases.jar pkgdir/CargarClase.class pkgdir res/*.png
